b8a386a4ad6adc190f8402145978eaaaac30ca9b
Elogeek <elodiechristin@gmail.com>
2021-01-29 13:53:56 +0100

    File php next

--- index.php
+++ index.php
@@ -6,6 +6,9 @@
  * --> Attention à bien fermer votre fichier si vous utilisez fopen()
  */
 // TODO Votre code ici.
+$content = "je suis une ligne de Lorem...";
+file_put_contents('texte.txt',$content);
+echo file_get_contents('texte.txt');
 
 
 /**
@@ -15,19 +18,26 @@
  * --> Attention: les chaînes de caractères doivent s'ajouter à la suite du contenu déjà existant, pas d'écrasement.
  */
 // TODO Votre code ici
-
+$array = ['0','1','2','3','4','5','6','7','8','9'];
+$file = fopen('texte.txt','a+b');
+fwrite($file, "$array\n");
+fseek($file, 0);
+fclose($file);
 
 /**
  * 5. Trouvez une solution pour afficher à l'aide d'un simple echo l'extension du fichier index.php
  */
 // TODO Votre code ici
-
+echo file_get_contents('texte.txt');
 
 
 /**
  * 6. Testez si le fichier 'toto' existe, sil n'existe pas, afficher un texte distant que ce fichier n'existe pas !
  */
 // TODO Votre code ici.
+if(!file_exists('toto.txt')) {
+    echo "Ce fichier n'existe pas";
+}
 
 
 /**
@@ -36,3 +46,14 @@
  * Attention, il y a un piège avec les pointeurs, et une manière très simple de procéder... réfléchissez...
  */
 // TODO Votre code ici si vous faites le bonus.
+$text = fopen("texte.txt","r");
+$textNew = file_get_contents("./texte.txt");
+$i = 0;
+while($car = fgetc($text)){
+    if($car === "a"){
+        $textNew[$i] = "@";
+    }
+    $i++;
+}
+file_put_contents("texte.txt",$textNew);
+fclose($text);
